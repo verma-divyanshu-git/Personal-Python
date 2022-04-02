553b963e4f64fd3818188543f69211c5cd6ac13a
verma-divyanshu-git <96394150+verma-divyanshu-git@users.noreply.github.com>
2022-04-02 22:57:55 +0530

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,9 @@
+# Create a greeting for your program
+print("Hi there! Are you confused with your band name? We are here to help!")
+# Taking input from the user
+city = input("Enter your city you grew up in: \n ")
+pet = input("Enter your pet's name: \n ")
+# Made sure input cursor appears on a new line
+# Combine them to show the band name
+band_name = city + pet
+print("Congratulations! Your new band name is " + band_name)
