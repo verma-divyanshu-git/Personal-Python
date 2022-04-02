ff83bb0bf931dbd6396752ab32818403bc16dbb7
verma-divyanshu-git <96394150+verma-divyanshu-git@users.noreply.github.com>
2022-04-02 23:00:15 +0530

    Update band_name_generator.py

--- band_name_generator.py
+++ band_name_generator.py
@@ -5,5 +5,5 @@ city = input("Enter your city you grew up in: \n ")
 pet = input("Enter your pet's name: \n ")
 # Made sure input cursor appears on a new line
 # Combine them to show the band name
-band_name = city + pet
+band_name = city + " " + pet
 print("Congratulations! Your new band name is " + band_name)
